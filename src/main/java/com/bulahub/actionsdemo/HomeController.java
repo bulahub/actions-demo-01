680a63028551f9a2ac04f0f9e7630012da210a2d
Neil <hireahoosier@yahoo.com>
2024-03-15 20:07:56 +0000

    run a build

--- src/main/java/com/bulahub/actionsdemo/HomeController.java
+++ src/main/java/com/bulahub/actionsdemo/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//a change to trigger build
+
 @Controller
 public class HomeController {
     @RequestMapping("/home")
