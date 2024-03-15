8ec6c0f45ddbd4350d68c6a630bbfdb4570a6a1d
Neil <hireahoosier@yahoo.com>
2024-03-15 20:10:02 +0000

    run a build

--- src/main/java/com/bulahub/actionsdemo/HomeController.java
+++ src/main/java/com/bulahub/actionsdemo/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
     @RequestMapping("/home")
     public @ResponseBody String getHome() {
+        System.out.println("change to run a build");
         return "Hello World!";
     }
 
